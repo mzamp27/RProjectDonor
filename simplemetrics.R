f38d84032489f5442663c597eb4fb30f96c4f71b
Michael Zampini <33523091+mzamp27@users.noreply.github.com>
2019-10-11 18:21:46 -0400

    Add files via upload

---
+++ simplemetrics.R
@@ -0,0 +1,5 @@
+# 3 Simple Fundraising Metrics for Nonprofits
+# 
+# 1. Annual Overall Rate of Growth in Donations (%)
+# 2. Annual Average Gift ($) & change in annual average gift amount
+# 3. Donor Lifetime Value ($)
